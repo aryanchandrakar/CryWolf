ae5f7ce28f8ca1f9d27b815989937ea4f6e20ced
Aryan Chandrakar <aryan.c.2000@gmail.com>
2022-04-04 18:55:15 +0530

    Update CryWolf.py

--- CryWolf.py
+++ CryWolf.py
@@ -21,7 +21,9 @@ def process_sniffed_packet(packet):
         print(Style.BRIGHT+target_url)
         # target url will have links that log you out of the session, or other cases that stops the process
         links_to_ignore= ["http://10.0.2.9/dvwa/logout.php","http://testphp.vulnweb.com/logout.php"]
+        data_dict = {"username": "admin", "password": "password","Login": "submit"}
         vuln_scanner = scanner.Scanner(target_url, links_to_ignore)
+        vuln_scanner.session.post("http://10.0.2.9/dvwa/login.php", data=data_dict)
         vuln_scanner.crawl()
         vuln_scanner.run_scanner()
 
